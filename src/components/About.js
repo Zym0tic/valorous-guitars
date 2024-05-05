e2c36c7769af40e5b585c092104cb0211e4b1db0
Zym0tic <lukerozon@gmail.com>
2024-05-05 02:04:26 -0400

    reduce about image size and center on desktop

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,14 @@
 import React from "react";
-//import about data
 import { aboutData } from "../data";
 
 const About = () => {
-  //destructure about data
   const { items } = aboutData;
+
   return (
     <section className="pt-[130px] lg:pt-[170px] lg:py-16" name="about">
       <div className="container mx-auto">
-        <div className="flex items-center">
-          {/* text */}
-          <div
-            className="flex-1 h-full xl:mt-30"
-          >
+        <div className="flex items-center justify-center"> {/* Center items horizontally */}
+          <div className="flex-1 h-full xl:mt-30">
             <div className="flex flex-col items-center">
               <div className="max-w-[700px] text-accent">
                 <ul className="py-7">
@@ -20,7 +16,7 @@ const About = () => {
                     const { subtitle, subtitle1, subtitle2, subtitle3, img } = item;
                     return (
                       <div key={index}>
-                        <img className="mb-5" src={img} alt="/" />
+                        <img className="mx-auto mb-5 lg:h-[80vh]" src={img} alt="/" />
                         <p className="mb-5">{subtitle}</p>
                         <p className="mb-5">{subtitle1}</p>
                         <p className="mb-5">{subtitle2}</p>
