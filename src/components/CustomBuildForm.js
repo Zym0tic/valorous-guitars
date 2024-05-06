90de90eceb44992c2d410bfb39491e26b55c5932
Zym0tic <lukerozon@gmail.com>
2024-05-06 01:57:08 -0400

    saving

--- src/components/CustomBuildForm.js
+++ src/components/CustomBuildForm.js
@@ -67,7 +67,7 @@ const GuitarForm = () => {
         name={fieldName}
         value={formData[fieldName]}
         onChange={handleChange}
-        className="form-input mt-1 block w-full bg-accent"
+        className="form-input mt-1 block w-full bg-accent text-accent2"
         placeholder={placeholder}
       />
     </div>
@@ -77,14 +77,14 @@ const GuitarForm = () => {
     <div className="container mx-auto px-4 pt-[130px] lg:pt-[170px]">
       <div className="py-7">
         <p>
-        If interested in a custom build, the best and most accurate way for me to quote you is by filling out this form. I take a more personalized, conversational approach to the design process. This allows me to fully understand your concept and build you the best possible instrument. If you’re unsure about any certain specifications, we can discuss options further or feel free to leave it to dealers choice, I will make my best recommendations.
+          If interested in a custom build, the best and most accurate way for me to quote you is by filling out this form. I take a more personalized, conversational approach to the design process. This allows me to fully understand your concept and build you the best possible instrument. If you’re unsure about any certain specifications, we can discuss options further or feel free to leave it to dealers choice, I will make my best recommendations.
         </p>
       </div>
-      <form onSubmit={handleSubmit} className="max-w-md mx-auto pb-7">
+      <form onSubmit={handleSubmit} className="max-w-md mx-auto">
         {renderSelectField("instrument", ["Guitar", "Bass"])}
-        {renderTextField("Body Style", "V, super strat, explorer..")}
-        {renderSelectField("Headstock Style", ["6-inline", "3+3", "Reversed"])}
-        {renderSelectField("Neck Construction", ["Bolt-on", "Neck-thru", "Set Neck"])}
+        {renderTextField("bodyStyle", "V, super strat, explorer..")}
+        {renderSelectField("headstockStyle", ["6-inline", "3+3", "Reversed"])}
+        {renderSelectField("neckConstruction", ["Bolt-on", "Neck-thru", "Set Neck"])}
         {renderTextField("numStrings", "Enter number of strings")}
         {renderSelectField("neckProfile", ["Slim C", "Asymmetrical", "Baseball bat"])}
         {renderTextField("woodSelection", "Enter wood selection")}
@@ -103,7 +103,7 @@ const GuitarForm = () => {
         {renderSelectField("binding", ["White ABS", "Curly Maple", "Matching Fretboard"])}
         {renderSelectField("inlays", ["Pearl Dots", "MOP Blocks", "Shark Fins"])}
         {renderTextField("notes", "Add notes")}
-        <p className='py-6'>Please allow 1-3 business days to complete your quote. Thanks for inquiring!</p>
+        <p>Please allow 1-3 business days to complete your quote. Thanks for inquiring!</p>
         <button type="submit" className="bg-accent hover:bg-header text-accent2 font-bold py-2 px-4 rounded">Submit</button>
       </form>
     </div>
