b23f261f8862d897f17e5b533f3a65861354a469
Zym0tic <lukerozon@gmail.com>
2024-05-19 20:43:56 -0400

    fix the labels on the form

--- src/components/CustomBuildForm.js
+++ src/components/CustomBuildForm.js
@@ -5,13 +5,14 @@ const GuitarForm = () => {
     instrument: '',
     bodyStyle: '',
     headstockStyle: '',
-    neckConstruction: '',
+    neckJoint: '',
     numStrings: '',
     orientation: '',
     neckProfile: '',
     bodyWood: '',
     neckWood: '',
     fretboardWood: '',
+    neckConstruction: '',
     scaleLength: '',
     fretboardRadius: '',
     fretAmmount: '',
@@ -63,9 +64,11 @@ const GuitarForm = () => {
     console.log(formData);
   };
 
-  const renderSelectField = (fieldName, options) => (
+  const renderSelectField = (fieldName, label, options) => (
     <div className="mb-4">
-      <label htmlFor={fieldName} className="block text-gray-700">{fieldName}</label>
+      <label htmlFor={fieldName} className="block text-gray-700">
+        {label}
+      </label>
       <select
         id={fieldName}
         name={fieldName}
@@ -73,20 +76,25 @@ const GuitarForm = () => {
         onChange={handleChange}
         className="form-select mt-1 block w-full bg-accent"
       >
-        <option value="">Select {fieldName}</option>
-        {options.map(option => (
-          <option key={option} value={option}>{option}</option>
+        <option value="">Select {label}</option>
+        {options.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
         ))}
       </select>
-      <div className='mt-4'>
-      {otherFields[fieldName] && renderTextField(``, `Enter ${fieldName} details`)}
+      <div className="mt-4">
+        {otherFields[fieldName] &&
+          renderTextField(``, `Enter ${label} details`)}
       </div>
     </div>
   );
 
-  const renderTextField = (fieldName, placeholder) => (
+  const renderTextField = (fieldName, label, placeholder) => (
     <div className="mb-4">
-      <label htmlFor={fieldName} className="block text-gray-700">{fieldName}</label>
+      <label htmlFor={fieldName} className="block text-gray-700">
+        {label}
+      </label>
       <input
         type="text"
         id={fieldName}
@@ -101,45 +109,163 @@ const GuitarForm = () => {
 
   return (
     <div className="container mx-auto px-4 pt-[130px] lg:pt-[170px]">
-      <div className="py-7">
-      </div>
+      <div className="py-7"></div>
       <form onSubmit={handleSubmit} className="max-w-md mx-auto pb-7">
-        {renderSelectField("instrument", ["Guitar", "Bass"])}
-        {renderSelectField("bodyStyle", ["Morgul-Blade", "Stormbringer", "Warmachine", "Other"])}
-        {renderSelectField("headstockStyle", ["Morgul-Blade", "Stormbringer", "Warmachine", "Other"])}
-        {renderSelectField("neckConstruction", ["Bolt-on", "Neck-thru", "Set Neck"])}
-        {renderSelectField("numStrings", ["4 (Bass)", "5 (Bass)", "6 (Guitar)", "7 (guitar)"])}
-        {renderSelectField("orientation", ["Right", "Left"])}
-        {renderSelectField("neckProfile", ["Slim C", "Medium C", "Fat C", "Speed", "Medium V", "Asymmetrical"])}
-        {renderSelectField("bodyWood", ["Alder", "Ash", "Black Limba", "Mahogany", "Walnut"])}
-        {renderSelectField("neckWood", ["Maple", "Roasted Maple", "Mahogany"])}
-        {renderSelectField("fretboardWood", ["Ebony", "Rosewood", "Maple"])}
-        {renderSelectField("neckConstruction", ["1 piece", "3 Piece", "5 Piece"])}
-        {renderSelectField("scaleLength", ["24.75\"", "25.5\"", "28.5\"", "30\"", "34\""])}
-        {renderSelectField("fretboardRadius", ["10\"", "12\"", "14\"", "10\"-14\" Compound", "12\"-16\" Compound"])}
-        {renderSelectField("fretAmmount", ["22", "24", "27"])}
-        {renderSelectField("fretSize", ["Medium", "Jumbo", "Super Jumbo"])}
-        {renderSelectField("fretMaterial", ["Nickel Silver", "Stainless Steel", "Evo-Gold"])}
-        {renderSelectField("bridge", ["Floyd Rose Original Tremolo", "Hipshot Fixed", "Hipshot Tone-A-Matic", "Kahler Quad Fixed", "Kahler 2300/7300"])}
-        {renderSelectField("nut", ["TUSQ XL", "Lok-N-Roll", "Floyd Locking Nut", "Bone"])}
-        {renderSelectField("tuners", ["Hipshot open gears", "Grover vintage", "gotoh locking tuners"])}
-        {renderSelectField("pickups", ["EMG", "Seymour Duncan", "Dimarzio", "Bare Knuckle"])}
-        {renderTextField("pickupsDetails", "Please enter more details")}
-        {renderTextField("controls", "Enter controls type")}
-        {renderTextField("jackPlate", "Enter jack plate type")}
-        {renderSelectField("sideDots", ["Luminlay", "Mother of Pearl (MOP)", "Plastic"])}
-        {renderSelectField("finishColor", ["Olympic White Satin", "Cherry Sunburst High Gloss Lacquer", "Clear Oiled Finish"])}
-        {renderSelectField("hardwareColor", ["Chrome", "Black", "Gold"])}
-        {renderSelectField("binding", ["White ABS", "Curly Maple", "Matching Fretboard"])}
-        {renderSelectField("inlays", ["Pearl Dots", "MOP Blocks", "Shark Fins"])}
-        <p className='pt-6'>Anything else you want. I won’t say yes to everything but doesn’t hurt to ask!</p>
-        {renderTextField("notes", "Add notes")}
-        <p className='py-6'>Please allow 1-3 business days to complete your quote. Thanks for inquiring!</p>
-        <button type="submit" className="bg-accent hover:bg-header text-accent2 font-bold py-2 px-4 rounded">Submit</button>
+        {renderSelectField('instrument', 'Instrument', ['Guitar', 'Bass'])}
+        {renderSelectField('bodyStyle', 'Body Style', [
+          'Morgul-Blade',
+          'Stormbringer',
+          'Warmachine',
+          'Other',
+        ])}
+        {renderSelectField('headstockStyle', 'Headstock Style', [
+          'Morgul-Blade',
+          'Stormbringer',
+          'Warmachine',
+          'Other',
+        ])}
+        {renderSelectField('neckJoint', 'Neck Joint', [
+          'Bolt-on',
+          'Neck-thru',
+          'Set Neck',
+        ])}
+        {renderSelectField('numStrings', 'Number of Strings', [
+          '4 (Bass)',
+          '5 (Bass)',
+          '6 (Guitar)',
+          '7 (Guitar)',
+        ])}
+        {renderSelectField('orientation', 'Orientation', ['Right', 'Left'])}
+        {renderSelectField('neckProfile', 'Neck Profile', [
+          'Slim C',
+          'Medium C',
+          'Fat C',
+          'Speed',
+          'Medium V',
+          'Asymmetrical',
+        ])}
+        {renderSelectField('bodyWood', 'Body Wood', [
+          'Alder',
+          'Ash',
+          'Black Limba',
+          'Mahogany',
+          'Walnut',
+        ])}
+        {renderSelectField('neckWood', 'Neck Wood', [
+          'Maple',
+          'Roasted Maple',
+          'Mahogany',
+        ])}
+        {renderSelectField('fretboardWood', 'Fretboard Wood', [
+          'Ebony',
+          'Rosewood',
+          'Maple',
+        ])}
+        {renderSelectField('neckConstruction', 'Neck Construction', [
+          '1 piece',
+          '3 Piece',
+          '5 Piece',
+        ])}
+        {renderSelectField('scaleLength', 'Scale Length', [
+          '24.75"',
+          '25.5"',
+          '28.5"',
+          '30"',
+          '34"',
+        ])}
+        {renderSelectField('fretboardRadius', 'Fretboard Radius', [
+          '10"',
+          '12"',
+          '14"',
+          '10"-14" Compound',
+          '12"-16" Compound',
+        ])}
+        {renderSelectField('fretAmmount', 'Fret Amount', ['22', '24', '27'])}
+        {renderSelectField('fretSize', 'Fret Size', [
+          'Medium',
+          'Jumbo',
+          'Super Jumbo',
+        ])}
+        {renderSelectField('fretMaterial', 'Fret Material', [
+          'Nickel Silver',
+          'Stainless Steel',
+          'Evo-Gold',
+        ])}
+        {renderSelectField('bridge', 'Bridge', [
+          'Floyd Rose Original Tremolo',
+          'Hipshot Fixed',
+          'Hipshot Tone-A-Matic',
+          'Kahler Quad Fixed',
+          'Kahler 2300/7300',
+        ])}
+        {renderSelectField('nut', 'Nut', [
+          'TUSQ XL',
+          'Lok-N-Roll',
+          'Floyd Locking Nut',
+          'Bone',
+        ])}
+        {renderSelectField('tuners', 'Tuners', [
+          'Hipshot open gears',
+          'Grover vintage',
+          'Gotoh locking tuners',
+        ])}
+        {renderSelectField('pickups', 'Pickups', [
+          'EMG',
+          'Seymour Duncan',
+          'Dimarzio',
+          'Bare Knuckle',
+        ])}
+        {renderTextField(
+          'pickupsDetails',
+          'Pickup Details',
+          'Please enter more details'
+        )}
+        {renderTextField('controls', 'Controls', 'Enter controls type')}
+        {renderTextField('jackPlate', 'Jack Plate', 'Enter jack plate type')}
+        {renderSelectField('sideDots', 'Side Dots', [
+          'Luminlay',
+          'Mother of Pearl (MOP)',
+          'Plastic',
+        ])}
+        {renderSelectField('finishColor', 'Finish Color', [
+          'Olympic White Satin',
+          'Cherry Sunburst High Gloss Lacquer',
+          'Clear Oiled Finish',
+        ])}
+        {renderSelectField('hardwareColor', 'Hardware Color', [
+          'Chrome',
+          'Black',
+          'Gold',
+        ])}
+        {renderSelectField('binding', 'Binding', [
+          'White ABS',
+          'Curly Maple',
+          'Matching Fretboard',
+        ])}
+        {renderSelectField('inlays', 'Inlays', [
+          'Pearl Dots',
+          'MOP Blocks',
+          'Shark Fins',
+        ])}
+        <p className="pt-6">
+          Anything else you want. I won’t say yes to everything but doesn’t hurt
+          to ask!
+        </p>
+        {renderTextField('notes', 'Add notes')}
+
+        <p className="py-6">
+          Please allow 1-3 business days to complete your quote. Thanks for
+          inquiring!
+        </p>
+        <button
+          type="submit"
+          className="bg-accent hover:bg-header text-accent2 font-bold py-2 px-4 rounded"
+        >
+          Submit
+        </button>
       </form>
     </div>
   );
 };
 
 export default GuitarForm;
-
