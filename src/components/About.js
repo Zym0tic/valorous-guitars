315b1c6a3de0a90d58303cfb5ce2592c27ac7412
Zym0tic <lukerozon@gmail.com>
2024-07-22 17:55:26 -0400

    add grid to guitars

--- src/components/About.js
+++ src/components/About.js
@@ -40,39 +40,49 @@ const About = () => {
                         <p className="mb-5">{paragraph2}</p>
                         <p className="mb-5">{paragraph3}</p>
                         <p className="mb-5">{paragraph4}</p>
-                        <div className="mt-[40px] uppercase">
-                        <p className="text-center italic">Stormbringer</p>
-                          <img
-                            className="mx-auto mb-5 lg:h-[80vh]"
-                            src={img1}
-                            alt="/"
-                          />
-                          <p className="text-center italic">Warmachine</p>
-                          <img
-                            className="mx-auto mb-5 lg:h-[80vh]"
-                            src={img2}
-                            alt="/"
-                          />
-                          <p className="text-center italic">Morgul Blade</p>
-                          <img
-                            className="mx-auto mb-5 lg:h-[80vh]"
-                            src={img3}
-                            alt="/"
-                          />
-                          <p className="text-center italic">Death Dealer</p>
-                          <img
-                            className="mx-auto mb-5 lg:h-[80vh]"
-                            src={img4}
-                            alt="/"
-                          />
-                          <p className="text-center italic">Carrionbird</p>
-                          <img
-                            className="mx-auto mb-5 lg:h-[80vh]"
-                            src={img5}
-                            alt="/"
-                          />
+                        <div id='guitars' className="mt-[40px] uppercase grid grid-cols-1 lg:grid-cols-3 gap-5">
+                          <div className="text-center">
+                            <p className="italic">Stormbringer</p>
+                            <img
+                              className="mx-auto mb-5"
+                              src={img1}
+                              alt="/"
+                            />
+                          </div>
+                          <div className="text-center">
+                            <p className="italic">Warmachine</p>
+                            <img
+                              className="mx-auto mb-5"
+                              src={img2}
+                              alt="/"
+                            />
+                          </div>
+                          <div className="text-center">
+                            <p className="italic">Morgul Blade</p>
+                            <img
+                              className="mx-auto mb-5"
+                              src={img3}
+                              alt="/"
+                            />
+                          </div>
+                          <div className="text-center">
+                            <p className="italic">Death Dealer</p>
+                            <img
+                              className="mx-auto mb-5"
+                              src={img4}
+                              alt="/"
+                            />
+                          </div>
+                          <div className="text-center">
+                            <p className="italic">Carrionbird</p>
+                            <img
+                              className="mx-auto mb-5"
+                              src={img5}
+                              alt="/"
+                            />
+                          </div>
                         </div>
-                        <div className="flex justify-center mt-[60px] lg;mt-[100px]">
+                        <div className="flex justify-center mt-[60px] lg:mt-[100px]">
                           <GetStarted />
                         </div>
                       </div>
