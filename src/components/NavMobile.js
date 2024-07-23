0690aeeb4c4ca5b5d86ad8065b48bbaa0229993d
Zym0tic <lukerozon@gmail.com>
2024-07-23 19:45:08 -0400

    close nav mobile when screen is wider than 1025

--- src/components/NavMobile.js
+++ src/components/NavMobile.js
@@ -1,14 +1,38 @@
-import React from 'react';
-//import Link
+import React, { useEffect } from 'react';
+// import Link
 import { Link } from 'react-router-dom';
-//import nav data
+// import nav data
 import { navData } from '../data';
-//import components
+// import components
 import Socials from './Socials';
 
 const NavMobile = (props) => {
-  //destructure nav data
+  // destructure nav data
   const { items } = navData;
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(min-width: 1025px)');
+    
+    const handleMediaQueryChange = (e) => {
+      if (e.matches) {
+        props.closeNavMobile(true);
+      }
+    };
+
+    // Add the listener
+    mediaQuery.addListener(handleMediaQueryChange);
+
+    // Check the initial state
+    if (mediaQuery.matches) {
+      props.closeNavMobile(true);
+    }
+
+    // Cleanup listener on component unmount
+    return () => {
+      mediaQuery.removeListener(handleMediaQueryChange);
+    };
+  }, [props]);
+
   return (
     <nav className="w-full h-full flex flex-col justify-evenly overflow-hidden">
       <ul className="flex flex-col justify-center items-center gap-y-4 py-6 mb-8 text-accent2">
